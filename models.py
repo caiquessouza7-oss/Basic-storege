c0433d1c4638faf45ee0122af2e12064a8e66e47
Caiqu3__ <caiquessouza7@gmail.com>
2026-08-24 19:51:24 -0300

    Fix indentation and update property names in Produto

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ class Produto:
     quantidade: int
 
     @property
-    def precisar_comprar(self) -> bool:
+    def precisa_comprar(self) -> bool:
         '''Define que o item precisa ser comprado se o estoque for 5 ou menos por hora
         ja que cada item deve ter uma classificacao especifica'''
         return self.quantidade <= 5
@@ -17,7 +17,14 @@ class Produto:
     def status_cor(self) -> str:
         '''Retorna uma cor baseada na quantidade de estoque'''
         if self.quantidade == 0:
-            return '#FFCCC'
+            return '#FFCCCC'
         elif self.quantidade <= 5:
             return '#FFF5CC'
         return '#CCFFCC'
+@dataclass
+class LayoutEstoque:
+    '''Estrutura do estoque'''
+    ruas: int = 4
+    prateleiras: int = 17
+    niveis: int = 5
+    blocos: int = 6
