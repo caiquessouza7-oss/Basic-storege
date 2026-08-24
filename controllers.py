758fcfc40ff74e02e8f8948953108f1fda261041
Caiqu3__ <caiquessouza7@gmail.com>
2026-08-24 19:50:10 -0300

    Refactor EstoqueController and add new methods

--- controllers.py
+++ controllers.py
@@ -1,9 +1,29 @@
-from models import Produto
+from models import Produto, LayoutEstoque
 
 class EstoqueController:
 
     def __init__(self, repository):
         self.repository = repository
+        self.layout = LayoutEstoque
+
+    def pesquisar_produto(self, termo):
+        '''Pesquisar pelo nome, codigo, localização'''
+
+        if not termo:
+            return []
+
+        termo = str(termo).lower()
+
+        todos_produtos = self.repositpory.listar_todos()
+
+        resultados = [
+            produto for produto in todos_produtos
+            if termo in str(produto.codigo).lower()
+            or termo in str(produto.descricao).lower()
+            or termo in str(produto.localizacao).lower()
+        ]
+
+        return resultados
 
     def registrar_novo_produto(self, codigo, descricao, localizacao, quantidade):
         if self.repository.buscar_por_codigo(codigo):
@@ -29,15 +49,27 @@ class EstoqueController:
             nova_quantidade = produto.quantidade + quantidade_movimento
         elif tipo == 'baixa':
             if quantidade_movimento > produto.quantidade:
-                return False, f'Estoque insuficiente. EStoque atual: {produto.quantidade}'
+                return False, f'Estoque insuficiente. Estoque atual: {produto.quantidade}'
             nova_quantidade = produto.quantidade - quantidade_movimento
         else:
             return False, 'Movimentaçao inválida'
 
         self.repository.atualizar_quantidade(codigo, nova_quantidade)
         return True, f'Movimentação efetuada. Saldo atual: {nova_quantidade}'
-    def obeter_lista_compras(self):
-        '''Produtos com estoque baixo'''
-        todos_produtos = self.repository.lisytar_todos()
-        return [p for p in todos_produtos if p.precisa_comprar]
     
+    def obter_lista_compras(self):
+        '''Produtos com estoque baixo'''
+        todos_produtos = self.repository.listar_todos()
+        return [p for p in todos_produtos if p.precisa_comprar]
+
+    def localizar_itens(self, termo):
+        '''Lista de itens buscado'''
+        return self.repository.localizar_produto_geral(termo)
+
+    def atualizar_layout_estoque(self, ruas, pratelerias, niveis, blocos):
+        '''Atualizar as configuracoes do estoque'''
+        self.layout.ruas = ruas
+        self.layout.prateleiras = pratelerias
+        self.layout.niveis = niveis
+        self.layout.blocos = blocos
+        return True, 'Layout do estoque atualizado'
