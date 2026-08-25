6fc9d7476d82f697de56c0a5ad06c77c67a0db18
Caiqu3__ <caiquessouza7@gmail.com>
2026-08-25 15:27:41 -0300

    Add foto_path to produtos table and update methods
    Added 'foto_path' field to the 'produtos' table and updated related methods to handle this new field.

--- repository.py
+++ repository.py
@@ -17,16 +17,17 @@ class ProdutoRepository:
             codigo TEXT PRIMARY KEY, 
             descricao TEXT, 
             localizacao TEXT,
-            quantidade INTEGER
+            quantidade INTEGER,
+            foto_path TEXT
             )
             ''')
 
     def salvar(self, produto):
         with self._conectar() as conn:
             conn.execute('''
-            INSERT INTO produtos (codigo, descricao, localizacao, quantidade)
-            VALUES(?, ?, ?, ?)
-            ''', (produto.codigo, produto.descricao, produto.localizacao, produto.quantidade))
+            INSERT INTO produtos (codigo, descricao, localizacao, quantidade, foto_path)
+            VALUES(?, ?, ?, ?, ?)
+            ''', (produto.codigo, produto.descricao, produto.localizacao, produto.quantidade, produto.foto_path))
 
     def deletar (self, codigo):
         with self._conectar() as conn:
@@ -36,35 +37,43 @@ class ProdutoRepository:
         with self._conectar() as conn:
             conn.execute('UPDATE produtos SET quantidade = ? WHERE codigo = ?', (nova_quantidade, codigo))
 
+    def atualizar_produto_completo(self, produto):
+        with self._conectar() as conn:
+            conn.execute('''
+            UPDATE produtos 
+            SET descricao = ?, localizacao = ?, quantidade = ?, foto_path = ?
+            WHERE codigo = ?
+            ''', (produto.descricao, produto.localizacao, produto.quantidade, produto.foto_path, produto.codigo))
+
     def buscar_por_codigo(self, codigo):
         with self._conectar() as conn:
             cursor = conn.cursor()
-            cursor.execute('SELECT codigo, descricao, localizacao, quantidade FROM produtos WHERE codigo =?',  (codigo,))
+            cursor.execute('SELECT codigo, descricao, localizacao, quantidade, foto_path FROM produtos WHERE codigo =?',  (codigo,))
             row = cursor.fetchone()
             if row:
                 from models import Produto
-                return Produto(codigo=row[0], descricao=row[1], localizacao=row[2], quantidade=row[3])
+                return Produto(codigo=row[0], descricao=row[1], localizacao=row[2], quantidade=row[3], foto_path=row[4] if len (row) > 4 else '')
             return None
 
     def listar_todos(self):
         with self._conectar() as conn:
             cursor = conn.cursor()
-            cursor.execute('SELECT codigo, descricao, localizacao, quantidade FROM produtos')
+            cursor.execute('SELECT codigo, descricao, localizacao, quantidade, foto_path FROM produtos')
             rows = cursor.fetchall()
-            return [Produto(codigo=r[0], descricao=r[1], localizacao=r[2], quantidade=r[3]) for r in rows]
+            return [Produto(codigo=r[0], descricao=r[1], localizacao=r[2], quantidade=r[3], foto_path=r[4]) for r in rows]
 
     def localizar_produto_geral(self, termo):
         with self._conectar() as conn:
             cursor = conn.cursor()
             query = '''
-                SELECT codigo, descricao, localizacao, quantidade
+                SELECT codigo, descricao, localizacao, quantidade, foto_path
                 FROM produtos
-                WHERE codigo LIKE ? OR descricao LIKE OR localizacao LIKE ?
+                WHERE codigo LIKE ? OR descricao LIKE ? OR localizacao LIKE ?
                 '''
             termo_busca = f'%{termo}%'
             cursor.execute(query, (termo_busca, termo_busca, termo_busca))
             rows = cursor.fetchall()
 
             from models import Produto
-            return [Produto(codigo=r[0], descricao=r[1], localizacao=r[2], quantidade=r[3]) for r in rows]
+            return [Produto(codigo=r[0], descricao=r[1], localizacao=r[2], quantidade=r[3], foto_path=r[4]) for r in rows]
         
