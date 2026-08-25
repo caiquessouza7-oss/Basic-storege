0a9eea072c50e7db57a1ae0dc6f031a03ccd7ac4
Caiqu3__ <caiquessouza7@gmail.com>
2026-08-25 15:26:22 -0300

    Add empty string parameter to product registration

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
     repo = ProdutoRepository('estoque.db')
     controller = EstoqueController(repo)
 
-    controller.registrar_novo_produto('99061501', 'EMBOLO ALUMINIO 30MM', 'R1.P1.N1', 7)
-    controller.registrar_novo_produto('99061502', 'EMBOLO ALUMINIO 30MM', 'R1.P1.N1', 3)
-    controller.registrar_novo_produto('99061503', 'EMBOLO ALUMINIO 30MM', 'R1.P1.N1', 0)
+    controller.registrar_novo_produto('99061501', 'EMBOLO ALUMINIO 30MM', 'R1.P1.N1', 7, '')
+    controller.registrar_novo_produto('99061502', 'EMBOLO ALUMINIO 30MM', 'R1.P1.N1', 3, '')
+    controller.registrar_novo_produto('99061503', 'EMBOLO ALUMINIO 30MM', 'R1.P1.N1', 0, '')
 
     app = QApplication(sys.argv)
     view = MapaEstoque(controller)
