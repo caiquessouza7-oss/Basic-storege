bd31bae205af6076ad46514cfbacaa1f13bfdb53
Caiqu3__ <caiquessouza7@gmail.com>
2026-08-25 15:27:05 -0300

    Enhance Produto class and LayoutEstoque methods
    Added 'foto_path' attribute to 'Produto' class and updated 'LayoutEstoque' class methods for managing street configurations.

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ class Produto:
     descricao: str
     localizacao: str
     quantidade: int
+    foto_path: str =""
 
     @property
     def precisa_comprar(self) -> bool:
@@ -24,7 +25,35 @@ class Produto:
 @dataclass
 class LayoutEstoque:
     '''Estrutura do estoque'''
-    ruas: int = 4
-    prateleiras: int = 17
-    niveis: int = 5
-    blocos: int = 6
+    configs_ruas: dict = None
+
+    @classmethod
+    def inicializar_layout(cls):
+        '''Layout inicial'''
+        if cls.configs_ruas is None:
+            cls.configs_ruas = {
+               f'Rua {i}' : {'prateleiras': 17, 'niveis': 5, 'blocos': 6} for i in range(4)
+            }
+    @classmethod
+    def obter_config(cls, num_rua: int):
+        cls.inicializar_layout()
+        return cls.configs_ruas.get(num_rua, {'prateleiras': 0, 'niveis': 0, 'blocos':0 })
+    
+    @classmethod
+    def adicionar_rua(cls, num_rua: int):
+        cls.inicializar_layout()
+        if num_rua not in cls.configs_ruas:
+            cls.configs_ruas[num_rua] = {'prateleiras': 17, 'niveis': 5, 'blocos': 6}
+            return True
+        return False
+
+    @classmethod
+    def remover_rua(cls, num_rua: int):
+        cls.inicializar_layout()
+        if num_rua in cls.configs_ruas:
+            del cls.configs_ruas[num_rua]
+            return True
+        return False
+    
+
+  
