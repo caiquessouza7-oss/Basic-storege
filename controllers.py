a0408f9757eabd85e64096005a50097f17ba43d9
Caiqu3__ <caiquessouza7@gmail.com>
2026-08-25 15:25:45 -0300

    Refactor EstoqueController with new methods

--- controllers.py
+++ controllers.py
@@ -14,7 +14,7 @@ class EstoqueController:
 
         termo = str(termo).lower()
 
-        todos_produtos = self.repositpory.listar_todos()
+        todos_produtos = self.repository.listar_todos()
 
         resultados = [
             produto for produto in todos_produtos
@@ -25,13 +25,22 @@ class EstoqueController:
 
         return resultados
 
-    def registrar_novo_produto(self, codigo, descricao, localizacao, quantidade):
+    def registrar_novo_produto(self, codigo, descricao, localizacao, quantidade, foto_path=""):
         if self.repository.buscar_por_codigo(codigo):
             return False, f'Produto com o codigo {codigo} já existe.'
 
         novo_produto = Produto(codigo, descricao, localizacao, quantidade)
         self.repository.salvar(novo_produto)
         return True, 'Produto cadastrado com sucesso'
+
+    def editar_dados_produto(self, codigo, nova_descricao, nova_localizacao, nova_quantidade, novo_foto_path=""):
+        produto_existente = self.repository.buscar_por_codigo(codigo)
+        if not produto_existente:
+            return False, 'Produto não encontrado'
+
+        produto_atualizado = Produto(codigo, nova_descricao, nova_localizacao, nova_quantidade, novo_foto_path)
+        self.repository.atualizar_produto_completo(produto_atualizado)
+        return True, 'Produto atualizado com sucesso'
 
     def excluir_produto(self, codigo):
         produto = self.repository.buscar_por_codigo(codigo)
@@ -66,10 +75,33 @@ class EstoqueController:
         '''Lista de itens buscado'''
         return self.repository.localizar_produto_geral(termo)
 
-    def atualizar_layout_estoque(self, ruas, pratelerias, niveis, blocos):
-        '''Atualizar as configuracoes do estoque'''
-        self.layout.ruas = ruas
-        self.layout.prateleiras = pratelerias
-        self.layout.niveis = niveis
-        self.layout.blocos = blocos
-        return True, 'Layout do estoque atualizado'
+    def listar_ruas_ativas(self):
+        self.layout.inicializar_layout()
+        # Retorna a lista de nomes cadastrados
+        return sorted(list(self.layout.configs_ruas.keys()))
+
+    def adicionar_nova_rua(self, nome_rua):
+        if not nome_rua.strip():
+            return False, 'O nome da rua não pode estar vazio.'
+            
+        sucesso = self.layout.adicionar_rua(nome_rua)
+        if sucesso:
+            return True, f'"{nome_rua}" adicionada com sucesso'
+        return False, f'A "{nome_rua}" já existe'
+
+    def excluir_rua(self, nome_rua):
+        sucesso = self.layout.remover_rua(nome_rua)
+        if sucesso:
+            return True, f'"{nome_rua}" removida com sucesso'
+        return False, f'"{nome_rua}" não encontrada'
+
+    def atualizar_layout_rua(self, nome_rua, prateleiras, niveis, blocos):
+        self.layout.inicializar_layout()
+        if nome_rua in self.layout.configs_ruas:
+            self.layout.configs_ruas[nome_rua] = {
+                'prateleiras': prateleiras,
+                'niveis': niveis,
+                'blocos': blocos
+            }
+            return True, f'Layout de "{nome_rua}" atualizado.'
+        return False, 'Rua não encontrada.'
